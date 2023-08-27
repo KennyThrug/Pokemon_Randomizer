2152a63449fc350331b91ded4b60c2a8d94c09a1
KennyThrug <kennyepstein6@gmail.com>
2023-08-27 16:33:10 -0500

    Trainer parties
    Converted Trainer Parties file to JSON File, as well as adding the wild_encounters json to a static file for use by game

--- randomizer/src/gen3/mod.rs
+++ randomizer/src/gen3/mod.rs
@@ -1,3 +1,4 @@
 pub mod emerald;
 pub mod frlg;
 pub mod trainers;
+pub mod wild_pokemon;
--- randomizer/src/gen3/trainers.rs
+++ randomizer/src/gen3/trainers.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use crate::settings;
+use json;
 
 use crate::pokemon::{self, Pokemon};
 
@@ -55,133 +57,14 @@ pub fn write_trainers_to_file(filename:String,trainers: Vec<Trainer>){
     }
     fs::write(filename,file_text).expect("Could not write trainer data");
 }
-pub fn get_unaltered_trainers() -> Vec<Trainer<'static>>{
-    let all_trainers: Vec<Trainer> = vec![
-        Trainer {trainer_name: "sParty_Sawyer1",pokemon: vec![TrainerPokemon{
-            iv: 0,
-            level: 21,
-            species: pokemon::get_species("Geodude"),
-            moves: Vec::new(),
-            held_items: ""
-    }]},
-    Trainer {trainer_name: "sParty_GruntAquaHideout1",pokemon: vec![TrainerPokemon{
-            iv: 0,
-            level: 32,
-            species: pokemon::get_species("Poochyena"),
-            moves: Vec::new(),
-            held_items: ""
-    }]},
-    Trainer {trainer_name: "sParty_GruntAquaHideout2",pokemon: vec![TrainerPokemon{
-            iv: 0,
-            level: 31,
-            species: pokemon::get_species("Zubat"),
-            moves: Vec::new(),
-            held_items: ""
-    },
-    TrainerPokemon{
-        iv: 0,
-        level: 31,
-        species: pokemon::get_species("Carvanha"),
-        moves: Vec::new(),
-        held_items: ""
-    }]},
-    Trainer {trainer_name: "sParty_GruntAquaHideout4",pokemon: vec![TrainerPokemon{
-            iv: 0,
-            level: 32,
-            species: pokemon::get_species("Carvahna"),
-            moves: Vec::new(),
-            held_items: ""
-    }]},
-    Trainer {trainer_name: "sParty_GruntSeafloorCavern1",pokemon: vec![TrainerPokemon{
-            iv: 0,
-            level: 36,
-            species: pokemon::get_species("Poochyena"),
-            moves: Vec::new(),
-            held_items: ""
-    }]},
-    Trainer {trainer_name: "sParty_GruntSeafloorCavern2",pokemon: vec![TrainerPokemon{
-            iv: 0,
-            level: 36,
-            species: pokemon::get_species("Carvahna"),
-            moves: Vec::new(),
-            held_items: ""
-    }]},
-    Trainer {trainer_name: "sParty_GruntSeafloorCavern3",pokemon: vec![TrainerPokemon{
-            iv: 0,
-            level: 36,
-            species: pokemon::get_species("Zubat"),
-            moves: Vec::new(),
-            held_items: ""
-    }]}
-    ];
-    all_trainers
-}
 
 fn read_all_trainers(filename: String){
-    let data = fs::read_to_string("decomp/pokeemerald-expansion/src/data/trainer_parties.h").expect("unable to read file");
-    let mut char_data = data.chars();
-    let mut results: Vec<Trainer> = Vec::new();
-    let mut cur_num_trainer = 0;
-    let mut cur_num_pokemon = 0;
-    let mut cur_word: String = "".to_string();
-    let mut move_cur = 0;
-    let mut iv_cur = false; let mut lvl_cur = false; let mut species_cur = false; let mut item_cur = false;
-    let cur_trainer = Trainer {
-        trainer_name: "",
-        pokemon: Vec::new()
-    };
-    for i in 0..data.len(){
-        if char_data.nth(i).unwrap() == ' ' || char_data.nth(i).unwrap() == '.'{
-            cur_word = "".to_string();
-        }
-        else if char_data.nth(i).unwrap() == ',' {
-            if iv_cur{
-                results[cur_num_trainer].pokemon.push(TrainerPokemon{
-                    iv: str::parse(&cur_word).unwrap(),
-                    species: &Pokemon { name:"", type1: pokemon::Type::None, type2: pokemon::Type::None, evo_stage: 0, total_stages: 0, status: pokemon::LegendStatus::Standard, generation: 0 },
-                    level: 0,
-                    moves: Vec::new(),
-                    held_items: ""
-                });
-            }
-            else if lvl_cur{
-                results[cur_num_trainer].pokemon[cur_num_pokemon].level = str::parse(&cur_word).unwrap();
-            }
-            else if species_cur{
-                results[cur_num_trainer].pokemon[cur_num_pokemon].species = pokemon::get_species(&cur_word);
-            }
-            else if item_cur{
-                results[cur_num_trainer].pokemon[cur_num_pokemon].held_items = &cur_word;
-            }
-            else if move_cur != 0{
-                results[cur_num_trainer].pokemon[cur_num_pokemon].moves[4 - move_cur] = cur_word.clone();
-                move_cur -= 1;
-            }
-            else if &cur_word == "iv"{
-                iv_cur = true;
-            }
-            else if &cur_word == "lvl"{
-                lvl_cur = true;
-            }
-            else if &cur_word == "species"{
-                species_cur = true;
-            }
-            else if &cur_word == "held_item"{
-                item_cur = true;
-            }
-            else if &cur_word == "moves"{
-                move_cur = 4;
-            }
-            cur_word = "".to_string();
-        }
-        else if char_data.nth(i).unwrap() == '['{
-
-        }
-        else if char_data.nth(i).unwrap() == ';'{
-            cur_num_pokemon = 0;
-        }
-        else{
-            cur_word = cur_word + &char_data.nth(i).unwrap().to_string();
-        }
-    }
+    let data = fs::read_to_string("data/emerald/trainer_parties.json").expect("unable to read file");
+    let data_json = json::parse(&data).unwrap();
+    println!("{}",data_json["sParty_Sawyer1"]);
+    //Read JSON file and put data in data
+    
+}
+pub fn shuffle_trainers(settings: &mut settings::Settings){
+    read_all_trainers("data/emerald/trainer_parties.json".to_string());
 }
