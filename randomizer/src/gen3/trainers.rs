f14fe45f67260bc6b875f4f5a3e35f1238d9383e
KennyThrug <kennyepstein6@gmail.com>
2023-08-30 09:03:51 -0500

    Trainers + Rework of Pokemon
    Changed "Pokemon" from Struct to Enum so that we aren't dealing with some weird const variable. Added CSV to read for data

--- randomizer/src/gen3/trainers.rs
+++ randomizer/src/gen3/trainers.rs
@@ -1,30 +1,32 @@
 use std::fs;
-use crate::settings;
-use json;
+use crate::{settings, pokemon::PokemonStats};
+use json::{self, JsonValue::Null};
 
-use crate::pokemon::{self, Pokemon};
+use crate::pokemon;
 
-pub struct Trainer<'a>{
-    pub trainer_name: &'a str,
-    pub pokemon: Vec<TrainerPokemon<'a>>
+pub struct Trainer{
+    pub trainer_name: String,
+    pub pokemon: Vec<TrainerPokemon>
 }
-pub struct TrainerPokemon<'a>{
+pub struct TrainerPokemon{
     iv: i32,
-    species: &'a Pokemon<'a>,
+    species: pokemon::Pokemon,
     level: i32,
     moves: Vec<String>,
-    held_items: &'a str,
+    held_items: String,
 }
-pub fn write_trainers_to_file(filename:String,trainers: Vec<Trainer>){
+pub fn write_trainers_to_file(filename:String,trainers: Vec<Trainer>,all_stats: &Vec<pokemon::PokemonStats>){
     let mut file_text = "".to_string();
     for cur_trainer in trainers{
-        let has_held_item = cur_trainer.pokemon[0].held_items.len() == 0;
-        let has_custom_moves = cur_trainer.pokemon[0].moves.len() == 0;
+        println!("{}",cur_trainer.trainer_name);
+        let has_held_item = cur_trainer.pokemon[0].held_items.len() != 0;
+        let has_custom_moves = cur_trainer.pokemon[0].moves.len() != 0;
+        println!("item: {} moves: {}",has_held_item,has_custom_moves);
         if has_custom_moves && has_held_item{
             file_text += &format!("static const struct TrainerMonItemCustomMoves {} [] = {{\n",cur_trainer.trainer_name).to_string();
             for cur_pkmn in cur_trainer.pokemon{
                 file_text += &format!("{{\n.iv = {},\n.lvl = {},\n.species = {},\n.heldItem = {},\n.moves = {{{},{},{},{}}}\n}},",
-            cur_pkmn.iv,cur_pkmn.level,pokemon::convert_name(&cur_pkmn.species),cur_pkmn.held_items,
+            cur_pkmn.iv,cur_pkmn.level,pokemon::format_pokemon_name(pokemon::get_pokemon_data(cur_pkmn.species,all_stats).pokemon_name),cur_pkmn.held_items,
             cur_pkmn.moves[0],cur_pkmn.moves[1],cur_pkmn.moves[2],cur_pkmn.moves[3])
             }
         }
@@ -33,7 +35,7 @@ pub fn write_trainers_to_file(filename:String,trainers: Vec<Trainer>){
             file_text += &format!("static const struct TrainerMonNoItemCustomMoves {} [] = {{\n",cur_trainer.trainer_name).to_string();
             for cur_pkmn in cur_trainer.pokemon{
                 file_text += &format!("{{\n.iv = {},\n.lvl = {},\n.species = {},\n.moves = {{{},{},{},{}}}\n}},",
-            cur_pkmn.iv,cur_pkmn.level,pokemon::convert_name(&cur_pkmn.species),
+            cur_pkmn.iv,cur_pkmn.level,pokemon::format_pokemon_name(pokemon::get_pokemon_data(cur_pkmn.species, all_stats).pokemon_name),
             cur_pkmn.moves[0],cur_pkmn.moves[1],cur_pkmn.moves[2],cur_pkmn.moves[3])
             }
         }
@@ -42,7 +44,7 @@ pub fn write_trainers_to_file(filename:String,trainers: Vec<Trainer>){
             file_text += &format!("static const struct TrainerMonItemDefaultMoves {} [] = {{\n",cur_trainer.trainer_name).to_string();
             for cur_pkmn in cur_trainer.pokemon{
                 file_text += &format!("{{\n.iv = {},\n.lvl = {},\n.species = {},\n.heldItem = {}}},",
-            cur_pkmn.iv,cur_pkmn.level,pokemon::convert_name(&cur_pkmn.species),cur_pkmn.held_items)
+            cur_pkmn.iv,cur_pkmn.level,pokemon::format_pokemon_name(pokemon::get_pokemon_data(cur_pkmn.species, all_stats).pokemon_name),cur_pkmn.held_items)
             }
         }
 
@@ -50,21 +52,57 @@ pub fn write_trainers_to_file(filename:String,trainers: Vec<Trainer>){
             file_text += &format!("static const struct TrainerMonNoItemDefaultMoves {} [] = {{\n",cur_trainer.trainer_name).to_string();
             for cur_pkmn in cur_trainer.pokemon{
                 file_text += &format!("{{\n.iv = {},\n.lvl = {},\n.species = {}}},",
-            cur_pkmn.iv,cur_pkmn.level,pokemon::convert_name(&cur_pkmn.species))
+            cur_pkmn.iv,cur_pkmn.level,pokemon::format_pokemon_name(pokemon::get_pokemon_data(cur_pkmn.species, all_stats).pokemon_name))
             }
         }
-        file_text += "\n}};\n\n"
+        file_text += "\n};\n\n"
     }
+    println!("text: {}",file_text);
     fs::write(filename,file_text).expect("Could not write trainer data");
 }
 
-fn read_all_trainers(filename: String){
-    let data = fs::read_to_string("data/emerald/trainer_parties.json").expect("unable to read file");
+fn read_all_trainers(filename: String,all_stats: &Vec<pokemon::PokemonStats>) -> Vec<Trainer>{
+    let data = fs::read_to_string(filename).expect("unable to read file");
     let data_json = json::parse(&data).unwrap();
-    println!("{}",data_json["sParty_Sawyer1"]);
+    let mut all_trainers: Vec<Trainer> = Vec::new();
+    for i in 0..1000{
+        if data_json["trainers"][i] == Null{break;}
+        let mut team : Vec<TrainerPokemon> = Vec::new();
+        for j in 0..6{
+            if(data_json["trainers"][i]["party"][j] == Null){break;}
+            let mut moves: Vec<String> = Vec::new();
+            if data_json["trainers"][i]["party"][j]["moves"] != Null{
+                for k in 1..5{
+                    moves.push(data_json["trainers"][i]["party"][j]["moves"][format!("move{}",k)].to_string());
+                }
+            }
+
+            team.push(TrainerPokemon {
+                 iv: data_json["trainers"][i]["party"][j]["iv"].as_i32().unwrap(),
+                 species: pokemon::get_pokemon_from_name(data_json["trainers"][i]["party"][j]["species"].to_string(), all_stats),
+                 level: data_json["trainers"][i]["party"][j]["lvl"].as_i32().unwrap(),
+                 moves:{
+                    moves
+                 },
+                 held_items: if data_json["trainers"][i]["party"][j]["heldItem"] == Null{
+                    "".to_string()
+                 }
+                 else{
+                    data_json["trainers"][i]["party"][j]["heldItem"].to_string()
+                 }
+                }
+            )
+        }
+        all_trainers.push(Trainer { 
+            trainer_name: data_json["trainers"][i]["name"].to_string(),
+             pokemon: team });
+    }
+    println!("len1:{}",all_trainers.len());
     //Read JSON file and put data in data
-    
+    return all_trainers;
 }
-pub fn shuffle_trainers(settings: &mut settings::Settings){
-    read_all_trainers("data/emerald/trainer_parties.json".to_string());
+pub fn shuffle_trainers(settings: &mut settings::Settings,all_stats: &Vec<pokemon::PokemonStats>){
+    let trainer_data = read_all_trainers("data/emerald/trainer_parties.json".to_string(),all_stats);
+    println!("len: {}",trainer_data.len());
+    write_trainers_to_file("decomp/pokeemerald-expansion/src/data/trainer_parties.h".to_string(), trainer_data,all_stats);
 }
--- randomizer/src/gen3/wild_pokemon.rs
+++ randomizer/src/gen3/wild_pokemon.rs
@@ -3,11 +3,10 @@ use crate::pokemon;
 use std::fs;
 
 const NUMBER_OF_ROUTES: usize = 124;
-pub fn randomize_wild_pokemon(settings: &mut settings::Settings){
+pub fn randomize_wild_pokemon(settings: &mut settings::Settings,pokemon_data: &Vec<pokemon::PokemonStats>){
     let data = fs::read_to_string("decomp/pokeemerald-expansion/src/data/wild_encounters.json").expect("unable to read file");
     let mut parsed_data = json::parse(&data).unwrap();
     for i in 0..NUMBER_OF_ROUTES{
-        println!("{}", i);
         for place in ["land_mons","water_mons","fishing_mons"]{
             for j in 0..30{ //Land Mons
                 if parsed_data["wild_encounter_groups"][0]["encounters"]
@@ -15,7 +14,7 @@ pub fn randomize_wild_pokemon(settings: &mut settings::Settings){
                     break;
                 }
                 parsed_data["wild_encounter_groups"][0]["encounters"]
-                [i][place]["mons"][j]["species"] = get_random_wild_pokemon(settings).into();
+                [i][place]["mons"][j]["species"] = get_random_wild_pokemon(settings,pokemon_data).into();
             }
         }
     }
@@ -25,21 +24,21 @@ pub fn randomize_wild_pokemon(settings: &mut settings::Settings){
 }
 
 //Gets a pokemon, checks if it can be put there, and returns the value
-fn get_random_wild_pokemon(settings: &mut settings::Settings) -> String{
+fn get_random_wild_pokemon(settings: &mut settings::Settings,pokemon_data: &Vec<pokemon::PokemonStats>) -> String{
     //Make Random Pokemon
-    let rand_val = settings::get_next_seed(0, pokemon::LIST_OF_POKEMON.len() as i32, settings);
-    let pokemon = &pokemon::LIST_OF_POKEMON[rand_val as usize];
+    let rand_val = settings::get_next_seed(0, pokemon_data.len() as i32, settings);
+    let pokemon = pokemon_data[rand_val as usize].clone();
     //Check if it is a valid pokemon
     if !settings.allow_pokemon_past_generation && pokemon.generation > 3{
-        return get_random_wild_pokemon(settings);
+        return get_random_wild_pokemon(settings,pokemon_data);
     }
     if !settings.allow_legends_in_wild_pool && pokemon.status == pokemon::LegendStatus::Legendary{
-        return get_random_wild_pokemon(settings);
+        return get_random_wild_pokemon(settings,pokemon_data);
     }
     if !settings.allow_megas_in_wild_pool && pokemon.status == pokemon::LegendStatus::Mega{
-        return get_random_wild_pokemon(settings);
+        return get_random_wild_pokemon(settings,pokemon_data);
     }
 
     //Return if it passes all checks
-    return pokemon::convert_name(pokemon);
+    return pokemon::format_pokemon_name(pokemon.pokemon_name);
 }
